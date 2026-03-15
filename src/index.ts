b4e4899bf0908962393e9f3ada5e14374aa1e243
guilherme-teixeira-gomes <guilherme.tgomes@hotmail.com>
2026-03-15 18:09:37 -0300

    feat: add swagger

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,15 @@ import cors from "cors";
 import "express-async-errors";
 import { AppDataSource } from "./data-source";
 import { routes } from "./routes/routes";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger/swagger.json'; 
 
 const app = express();
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
 app.use(routes);
 app.get("/", (req, res) => {
   return res.json({ message: "API Oficina funcionando 🚗" });
@@ -16,8 +20,9 @@ app.get("/", (req, res) => {
 
 AppDataSource.initialize()
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Servidor rodando na porta 3000");
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor rodando na porta ${PORT}`);
+      console.log(`📚 Swagger disponível em http://localhost:${PORT}/api-docs`);
     });
   })
   .catch((error) => console.log(error));
